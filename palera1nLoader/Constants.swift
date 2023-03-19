dc550cad8c420b1e8cd7468010bb6d685643b1ac
afastaudir8 <thehistorykidd@gmail.com>
2023-03-19 15:19:21 -0400

    fix the 16 colors

--- palera1nLoader/Constants.swift
+++ palera1nLoader/Constants.swift
@@ -12,7 +12,7 @@ func palera1nColorGradients() -> [Color] {
     if UIDevice.current.systemVersion.contains("15") {
         return [.init(hex: "B284BE"), .init(hex: "F81FFF"), .init(hex: "F345FF")] // iOS 15 color palette
     } else if UIDevice.current.systemVersion.contains("16") {
-        return [.init(hex: "1440e2"), .init(hex: "8dd1c5"), .init(hex: "b6d6ed"), .init(hex: "269dd4"), .init(hex: "55cdfc")] // iOS 16 color palette
+        return [.init(hex: "1ab1ec"), .init(hex: "8dd1c5"), .init(hex: "b6d6ed"), .init(hex: "269dd4"), .init(hex: "55cdfc")] // iOS 16 color palette
     }
     return [.init(hex: "000000")]
 }
